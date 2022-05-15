adb53b36ff66f92dc4a13d733701e3cb39897465
Thorben Primke <thorben@pinterest.com>
2022-05-15 10:51:34 -0500

    adds entitlement

--- src/expo-plugin/withCredentialSharing.ts
+++ src/expo-plugin/withCredentialSharing.ts
@@ -1,9 +1,13 @@
+
 import { 
     withPlugins,
     AndroidConfig,
     ConfigPlugin,
-    createRunOncePlugin
+    createRunOncePlugin,
+    withEntitlementsPlist
 } from '@expo/config-plugins';
+import type { ExpoConfig } from '@expo/config-types';
+import type { JSONObject } from '@expo/json-file';
 // eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-assignment
 const pkg = require('../../../package.json');
 
@@ -17,6 +21,45 @@ type Props = {
       otherValue: string;
   }
 };
+
+type MutateEntitlementsPlistAction = (expo: ExpoConfig, entitlements: JSONObject) => JSONObject;
+
+export function createEntitlementsPlugin(
+  action: MutateEntitlementsPlistAction,
+  name: string
+): ConfigPlugin {
+  const withUnknown: ConfigPlugin = config =>
+    withEntitlementsPlist(config, async config => {
+      config.modResults = await action(config, config.modResults);
+      return config;
+    });
+  if (name) {
+    Object.defineProperty(withUnknown, 'name', {
+      value: name,
+    });
+  }
+  return withUnknown;
+}
+
+
+export const withKeychainSharingGroups = createEntitlementsPlugin(
+  setKeychainSharingGroups,
+  'withKeychainSharingGroups'
+);
+
+export function setKeychainSharingGroups(
+  config: ExpoConfig,
+  { 'keychain-access-groups': _, ...entitlementsPlist }: JSONObject
+): JSONObject {
+  if (config.ios?.associatedDomains) {
+    return {
+      ...entitlementsPlist,
+      'keychain-access-groups': ['$(AppIdentifierPrefix)com.pinterest.enterprise'],
+    };
+  }
+
+  return entitlementsPlist;
+}
 
 const withCredentialSharing: ConfigPlugin<Props> = (config, props = {}) => {
   const androidPermissions = [];
@@ -33,7 +76,8 @@ const withCredentialSharing: ConfigPlugin<Props> = (config, props = {}) => {
   return withPlugins(
       config,
       [
-        [AndroidConfig.Permissions.withPermissions, androidPermissions]
+        [AndroidConfig.Permissions.withPermissions, androidPermissions],
+        withKeychainSharingGroups
       ]
     );
 };
