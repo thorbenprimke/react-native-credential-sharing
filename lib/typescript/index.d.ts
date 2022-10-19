eb1aaffef986cb9c4125139be91325ddb4892431
Thorben Primke <thorben@pinterest.com>
2022-10-19 13:51:14 -0500

    updated lib

--- lib/typescript/index.d.ts
+++ lib/typescript/index.d.ts
@@ -1,2 +1,14 @@
-export declare function multiply(a: number, b: number): Promise<number>;
+export declare type CredentialResponse = {
+    accessToken: string;
+    id?: string;
+    username?: string;
+    name?: string;
+};
+export declare function getSharedCredentials(android?: {
+    uri: string;
+}, iOS?: {
+    service: string;
+    account: string;
+    accessGroup: string;
+}): Promise<CredentialResponse[]>;
 //# sourceMappingURL=index.d.ts.map
